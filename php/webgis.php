8c1ff5c7cc76846b8b6296a7925bf6979d3bd5ec
l-bode05 <lulu_isa@web.de>
2015-07-27 23:28:48 +0200

    Measurement0.9.1

--- php/webgis.php
+++ php/webgis.php
@@ -30,7 +30,7 @@ else{
 	</head>
 	
 	<body class="claro">
-		<!--<div id="map" data-dojo-type="dijit/layout/ContentPane" data-dojo-props="region:'center'">
+		<div id="map" data-dojo-type="dijit/layout/ContentPane" data-dojo-props="region:'center'">
 			<div id="HomeButton"></div>
 			<div id="map-submitted" class="map-processing"><img src="../img/map/submitted.png" width="200" height="200"></div>
 			<div id="map-loading" class="map-processing"><img src="../img/map/loading.gif" width="200" height="200"></div>
@@ -49,26 +49,13 @@ else{
 				<div style="width:380px; height:200px; overflow:auto;"> 
 					<div id="titlePane" data-dojo-type="dijit/TitlePane" data-dojo-props="title:'Measurement', closable:'false', open:'false'">
 						<div id="measurementDiv"></div>
-						<span style="font-size:smaller;padding:5px 5px;">Press <b>CTRL</b> to enable snapping.testtestestestestestsetstetst</span>
+						<span style="font-size:smaller;padding:5px 5px;">Press <b>CTRL</b> to enable snapping.</span>
 					</div>
 				</div>
 			</div>
-		</div>-->
+		</div>
 		
-		 <div id="mainWindow" data-dojo-type="dijit/layout/BorderContainer" data-dojo-props="design:'headline',gutters:false"
-    style="width:100%; height:100%;">
-      <div id="map" data-dojo-type="dijit/layout/ContentPane" data-dojo-props="region:'center'">
-        <div style="position:absolute; right:20px; top:10px; z-Index:999;">
-          <div id="titlePane" data-dojo-type="dijit/TitlePane" data-dojo-props="title:'Measurement', closable:'false', open:'false'">
-            <div id="measurementDiv"></div>
-            <span style="font-size:smaller;padding:5px 5px;">Press <b>CTRL</b> to enable snapping.</span>
-          </div>
-        </div>
-      </div>
-    </div>
-
 		
-
 
 		
 		<div id="mapdetails">
