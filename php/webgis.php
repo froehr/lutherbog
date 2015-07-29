01c5bd9bd3eefc8ded4c5c99459263d66edeb409
l-bode05 <lulu_isa@web.de>
2015-07-29 20:09:58 +0200

    Measurement0.9.4

--- php/webgis.php
+++ php/webgis.php
@@ -46,12 +46,11 @@ else{
 			</div>
 			
 			<div id="Measurement">
-				<div data-dojo-type="dijit/TitlePane" data-dojo-props="title:'Measurement', closable:false, open:false">
-					<div data-dojo-type="dijit/layout/ContentPane" style="width:380px; height:200px; overflow:auto;">
-						<div id="measurementDiv"></div>
-						<span style="font-size:smaller;padding:5px 5px;">Press <b>CTRL</b> to enable snapping.</span>
-					</div>
+				<div id="titlePane" data-dojo-type="dijit/TitlePane" data-dojo-props="title:'Measurement', closable:'false', open:'false'">
+					<div id="measurementDiv"></div>
+					<span style="font-size:smaller;padding:5px 5px;">Press <b>CTRL</b> to enable snapping.</span>
 				</div>
+
 			</div>
 		</div>
 		
