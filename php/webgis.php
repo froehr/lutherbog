1ff5ce359617436fc2cb859c906dbad96a105edd
l-bode05 <lulu_isa@web.de>
2015-08-15 18:21:03 +0200

    GetCapabilities der einzelnen Flüge

--- php/webgis.php
+++ php/webgis.php
@@ -62,8 +62,8 @@ else{
 		<div id="mapdetails">
 				<div id="layerList">
 					<h3>Verfügbare Layer:</h3>
-					<input type='checkbox' id='sites' value=0 /> Sites </br>
-					<input type='checkbox' id='hoehe' value=0 /> Höhenmodel </br>
+					<input type='checkbox' id='sites' value=0 /> Messpunkte (Sites)</br>
+					<input type='checkbox' id='hoehe' value=0 /> Höhenmodel (DTM) </br>
 					<input type="range" id="hoehe_opacity" min="0" max="1" step="0.01"> <input type="text" id="hoehe_opacity_value" size = "4" value = "50%" readonly> </br>
 					
 					<input type='checkbox' id='isolines' value=0 /> Höhenlinien </br>
@@ -72,21 +72,57 @@ else{
                                 <p>Prozess starten</p>
                          </div></br></br>
 					
-					<input type='checkbox' id='ortho_tiles' value=1 /> Luftbilder Tiles 2006</br>
+					<input type='checkbox' id='ortho_tiles' value=1 /> Luftbilder 2006 (Tiles) </br>
 					<!--<input type="range" id="ortho_tiles_opacity" min="0" max="1" step="0.01"> <input type="text" id="ortho_tiles_opacity_value" size = "4" value = "50%" readonly></br>-->
-					<input type='checkbox' id='ortho_merged' value=1 /> Luftbilder Merged 2006</br>
+					<input type='checkbox' id='ortho_merged' value=1 /> Luftbilder 2006 (Merged) </br>
 					<!--<input type="range" id="ortho_merged_opacity" min="0" max="1" step="0.01"> <input type="text" id="ortho_merged_opacity_value" size = "4" value = "50%" readonly></br>-->
-					<input type='checkbox' id='ortho_uav' value=1 /> IfGi UAV Luftbilder 2013</br>
+					<!--<input type='checkbox' id='ortho_uav' value=1 /> IfGi UAV Luftbilder 2013</br>-->
 					<!--<input type="range" id="ortho_uav_opacity" min="0" max="1" step="0.01"> <input type="text" id="ortho_uav_opacity_value" size = "4" value = "50%" readonly></br></br>-->
-					<input type='checkbox' id='uas_flight' value=1 /> alle UAS Flights</br>
+					</br>
+					<input type='checkbox' id='uas_flight' value=1 /> alle Flüge 2015</br>
 					
-					<input type='checkbox' id='flug1' value=1 /> Flug 1</br>
-					<input type='checkbox' id='flug2' value=1 /> Flug 2</br>
-					<input type='checkbox' id='flug3' value=1 /> Flug 3</br>
-					<input type='checkbox' id='flug4' value=1 /> Flug 4</br>
-					<input type='checkbox' id='flug5' value=1 /> Flug 5</br>
+					
+					<input type='checkbox' id='flug1' value=1 /> <a onclick=window.open("https://geo-arcgis.uni-muenster.de:6443/arcgis/services/LutherBog/Flight1/MapServer/WFSServer?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetCapabilities") title="GetCapabilities" style="cursor: pointer"> Flug 1</a>
+					</br>
+					<input type='checkbox' id='flug2' value=1 /> <a onclick=window.open("https://geo-arcgis.uni-muenster.de:6443/arcgis/services/LutherBog/Flight2/MapServer/WFSServer?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetCapabilities") title="GetCapabilities" style="cursor: pointer"> Flug 2</a>
+					</br>
+					<input type='checkbox' id='flug3' value=1 /> <a onclick=window.open("https://geo-arcgis.uni-muenster.de:6443/arcgis/services/LutherBog/Flight3/MapServer/WFSServer?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetCapabilities") title="GetCapabilities" style="cursor: pointer"> Flug 3</a>
+					</br>
+					<input type='checkbox' id='flug4' value=1 /> <a onclick=window.open("https://geo-arcgis.uni-muenster.de:6443/arcgis/services/LutherBog/Flight4/MapServer/WFSServer?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetCapabilities") title="GetCapabilities" style="cursor: pointer"> Flug 4</a>
+					</br>
+					<input type='checkbox' id='flug5' value=1 /> <a onclick=window.open("https://geo-arcgis.uni-muenster.de:6443/arcgis/services/LutherBog/Flight5/MapServer/WFSServer?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetCapabilities") title="GetCapabilities" style="cursor: pointer"> Flug 5</a>
+					</br>
+					</br>
 					 
-					
+					<div id="list1" class="dropdown-check-list" tabindex="100">
+						<span class="anchor">Wähle Flug</span>
+						<ul class="items">
+							<li><input type='checkbox' id='flug1' value=1 /> Flug 1</li>							
+							<li><input type='checkbox' id='flug2' value=1 /> Flug 2</li>
+							<li><input type='checkbox' id='flug3' value=1 /> Flug 3</li>
+							<li><input type='checkbox' id='flug4' value=1 /> Flug 4</li>
+							<li><input type='checkbox' id='flug5' value=1 /> Flug 5</li>
+						</ul>
+					</div>
+					</br>
+					</br>
+
+					<script type="text/javascript">
+
+						var checkList = document.getElementById('list1');
+						checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+							if (checkList.classList.contains('visible'))
+								checkList.classList.remove('visible');
+							else
+								checkList.classList.add('visible');
+						}
+
+						checkList.onblur = function(evt) {
+							checkList.classList.remove('visible');
+						}
+					</script>
+						
+
 					
 					<input type='checkbox' id='flooded_area_part' value=1 /> Überflutete Teilgebiete</br>
 					<input type="range" id="flooded_area_gauge_part" min="480" max="494" step="0.01"> <input type="text" id="flooded_area_gauge_value_part" size = "4" value = "487m" readonly></br>
@@ -107,11 +143,11 @@ else{
 				<div id="mainWindow" data-dojo-type="dijit/layout/BorderContainer" data-dojo-props="design:'headline',gutters:false" style="width:100%; height:100%;">
 					<!--<div data-dojo-type="dijit/layout/ContentPane" id="rightPane" data-dojo-props="region:'right'">-->
 						<!--<div style='margin-left:5px;'>-->
-							<p> Add zipped Shapefile to the map. </p>
+							<p> Shapefile hinzufügen (Add zipped shp. file) </p>
 							<form enctype="multipart/form-data" method="post" id="uploadForm">
 								<div class="field">
 									<label class="file-upload">
-										<!--<span><strong><p>-->Add File<!--</p></strong></span>-->
+										<!--<span><strong><p>-->Füge hinzu<!--</p></strong></span>-->
 										<input type="file" name="file" id="inFile" />
 									</label>
 								</div>
